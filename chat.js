9d903c83f3eff5458e78b6b224455e66e8d219dd
unknown <31709@EBI293GFWW.corp.hexaware.com>
2017-02-10 11:44:34 +0530

    first commit from local

---
+++ chat.js
@@ -0,0 +1,115 @@
+var socket = io();
+var userName;
+
+function joinChat(){
+	var user = $('#user').val();
+	console.log("user ", user);
+	Cookies.set("userName",user);
+	socket.emit('newUser',user);
+}
+
+function notifyTyping(){
+	var user = Cookies.get("userName");
+	console.log("user from notifyTyping " +user);
+	socket.emit('notifyUser',user);
+}
+
+function submitfunction(){
+	var from =  Cookies.get("userName"); 
+	console.log('from in submitfunction '+ userName);
+	var message = $('#m').val();
+	if(message != null || message != ''){
+		socket.emit('chatMessage',from,message);
+		var messageDiv =document.getElementById("messageDiv");
+		/*console.log("$('#messageDiv').scrollHeight "+messageDiv.scrollHeight);
+		console.log("$('#messageDiv').clientHeight "+messageDiv.clientHeight );
+		console.log("$('#messageDiv').scrollTop "+ messageDiv.scrollTop);	*/
+		var isScrolledToBottom = messageDiv.scrollHeight - messageDiv.clientHeight <= messageDiv.scrollTop + 1;
+		if(!isScrolledToBottom){
+			messageDiv.scrollTop = (messageDiv.scrollHeight - messageDiv.clientHeight) ;	
+		}    		
+	}
+	$('#m').val('').focus();
+	return false;
+}
+
+function clearError(){
+	$("#userVerify").empty();
+}
+function leaveChat(){
+	console.log("leaving chat bye bye" +Cookies.get("userName"));
+	socket.emit("disconnectServer",Cookies.get("userName"));
+	window.location = "/" ;
+}
+
+socket.on('addUserName',function(value,user){
+	console.log("value " +value);
+	console.log("user " +user);
+	if(value === "success"){
+		console.log("inside success");
+		uName = Cookies.get("userName");
+		console.log(userName+ 'value of username');
+		if(uName === user){
+			window.location.href = '/startChat?userName='+user;	
+		}else{
+			console.log("user name not valid");
+		}
+		
+	}else{
+		$("#userVerify").append("<p> cannot add user.User Already exists");
+	}
+});
+
+socket.on('serverChatMessage',function(from,msg){
+	console.log("from " +from);
+	console.log("message "+ msg);
+	console.log("me "+userName);
+	var me = Cookies.get("userName"); 
+	console.log("me "+ me);
+	var color = (from == me) ? 'ffc4c4' : '0addf5';
+	console.log("color "+ color);
+	var from  = (from ==me) ? 'Me' : from;
+	console.log("from " + from);
+	$('#messages').append('<li><b style="color:' +color+ '"> ' + from + '</b>: ' + msg + '</li>');
+});
+
+socket.on('notifyServerUser', function(user){
+  var me = Cookies.get("userName"); 
+  console.log('inside notify user me'+ me);
+  console.log('inside notify user received'+ me);
+  if(user != me) {
+    $('#notifyUser').text(user + ' is typing ...');
+  }
+  setTimeout(function(){ $('#notifyUser').text(''); }, 1000);;
+});
+
+
+socket.on('listOfUsers',function(usersList){
+	console.log("listOfUsers.............................. first" +usersList);
+	var me = Cookies.get("userName");
+	$("#activeUsers").empty();
+	for(var s=0;s<usersList.length;s++){
+		if(usersList[s] === me){
+			usersList.splice(s,1);
+		}
+	}
+	usersList.forEach(function(values){
+		$("#activeUsers").append('<li>' +values +'</li>')	;
+	});
+
+	clearDuplicates();
+
+	function clearDuplicates(){
+		var seen = {};
+		$('li').each(function() {
+		    var txt = $(this).text();
+		    if (seen[txt])
+		        $(this).remove();
+		    else
+		        seen[txt] = true;
+		});
+	}
+	usersList = [];
+
+	console.log("listOfUsers.............................." +usersList);	
+});
---
+++ index.js
@@ -0,0 +1,103 @@
+	var express = require('express');
+
+var app = express();
+
+var http = require('http').Server(app);
+
+var io  = require('socket.io')(http);
+
+var path = require('path');
+
+var userNames = [];
+var checkUsers = [];
+var cutoffLogin = false;
+var currentUserName ;
+
+app.get('/',function(req,res){
+	app.use(express.static(path.join(__dirname)));
+	res.sendFile(path.join(__dirname,'../MultiChatApp','index.html'));	
+});
+
+app.get('/startChat',function(req,res){
+	app.use(express.static(path.join(__dirname)));
+	console.log("req.query.id is set to " + req.query.userName);
+	currentUserName = req.query.userName;
+	res.sendFile(path.join(__dirname,'../MultiChatApp','chat.html'));	
+});
+
+
+io.on('connection',function(socket){
+	console.log('connection established');
+
+	io.emit('listOfUsers',userNames);
+	
+	socket.on('disconnect',function(){
+		console.log('user disconnected');
+	});
+
+	socket.on("disconnectServer",function(userName){
+	console.log("inside disconnectServer event ", userName );
+	for (var i=userNames.length-1; i>=0; i--) {
+    	if (userNames[i] === userName) {
+        	userNames.splice(i, 1);
+    	}
+	}
+		io.emit('listOfUsers',userNames);
+		socket.disconnect();
+	});
+
+	socket.on('chatMessage',function(from,msg){
+		console.log('from '+ from);
+		console.log('chatMessage '+msg);
+		io.emit('serverChatMessage',from,msg);
+	});
+
+	socket.on('notifyUser',function(user){
+		io.emit('notifyServerUser',user);
+	});
+
+	socket.on('newUser',function(user){
+		console.log('userName ', user);
+	
+		for(var i=0;i<userNames.length;i++){	
+			if(user == userNames[i]){
+				console.log("same");
+				checkUsers.push("same");
+				break;
+				}
+			else{
+				console.log("different");
+				checkUsers.push("different");
+				}
+			}
+	console.log("checkUsers "+checkUsers);
+	console.log("checkUsers.length "+checkUsers.length);
+
+	for(var j=0;j<checkUsers.length;j++){
+		if(checkUsers[j] =="same"){
+			console.log("same user");
+			cutoffLogin = true;
+		}else{
+			console.log("different user ");
+		}
+	};
+	console.log('cutoffLogin' + cutoffLogin);
+	
+	if(!cutoffLogin){
+		console.log("inside adding user ");
+		userNames.push(user);
+		console.log(userNames+' userNames');
+		io.emit("addUserName","success",user);
+	}else{
+		console.log("cannot add user ");
+		io.emit("addUserName","failure");
+	}
+	checkUsers = [];
+	cutoffLogin = false;
+	});
+});
+
+
+http.listen(3000,function(){
+	console.log('node server listning on port 3000');
+});
